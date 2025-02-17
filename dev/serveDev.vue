8427139e55c179701a13dd0a47564108b09a8595
Andreé Cuenca <andre@polimundo.com.ec>
2025-02-17 13:25:14 -0500

    feat: Modificar serveDec.vue para poder modificar los props fácilmente

--- dev/serveDev.vue
+++ dev/serveDev.vue
@@ -1,17 +1,55 @@
 <script setup lang="ts">
-import { ref, useTemplateRef } from 'vue';
+import { computed, ref, useTemplateRef } from 'vue';
 import TravelDatepicker from '../src/components/TravelDatePicker.vue';
 import { CountType, PublicMethods } from '../src/types';
+
+const defaultMinDate = new Date();
+defaultMinDate.setDate(defaultMinDate.getDate() + 2);
+const defaultMaxDate = new Date();
+defaultMaxDate.setMonth(defaultMaxDate.getMonth() + 12);
 
 const datePicker = useTemplateRef<PublicMethods>('travel-datepicker');
 
 const startD = ref('');
 const endD = ref('');
 
-const minDate = new Date();
-minDate.setDate(minDate.getDate() + 2);
-const maxDate = new Date();
-maxDate.setMonth(maxDate.getMonth() + 12);
+const countType = ref<CountType>(CountType.DAY);
+const enableMinDate = ref<Boolean>(false);
+const minDate = ref<Date|string>(defaultMinDate);
+const enableMaxDate = ref<Boolean>(false);
+const maxDate = ref<Date|string>(defaultMaxDate);
+
+const formattedMinDate = computed(() => {
+  if (!enableMinDate.value || !minDate.value) {
+    return undefined;
+  }
+
+  if (typeof(minDate.value) !== 'string') {
+    return defaultMinDate;
+  }
+
+  return getFormattedDateForComp(minDate.value);
+
+})
+
+const formattedMaxDate = computed(() => {
+  if (!enableMaxDate.value || !maxDate.value) {
+    return undefined;
+  }
+
+  if (typeof(maxDate.value) !== 'string') {
+    return defaultMaxDate;
+  }
+
+  return getFormattedDateForComp(maxDate.value);
+
+})
+
+const getFormattedDateForComp = (dateText: string) => {
+  const [year, month, day] = dateText.split('-');
+
+  return new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+}
 
 const onDateInputClick = () => {
   if (datePicker.value) {
@@ -71,24 +109,91 @@ const formatDateString = (dateObj: Date) => {
         >
         <travel-datepicker
           ref="travel-datepicker"
-          :count-type="CountType.NIGHT"
+          :count-type="countType"
+          :min-date="formattedMinDate"
+          :max-date="formattedMaxDate"
           @update:model-value="handleUpdateOnDatePicker"
         />
+      </div>
+
+      <h2>Props</h2>
+      <div class="props">
+        <div class="form">
+          <div class="prop-input">
+            <label for="countType">countType</label>
+            <select v-model="countType" name="countType">
+              <option :value="CountType.DAY">Days</option>
+              <option :value="CountType.NIGHT">Nights</option>
+            </select>
+          </div>
+          <div class="prop-input">
+            <label for="enableMinDate">Enable minDate</label>
+            <input v-model="enableMinDate" name="enableMinDate" type="checkbox">
+          </div>
+          <div :class="{'prop-input': true, 'disable': !enableMinDate }">
+            <label for="minDate">minDate</label>
+            <input v-model="minDate" name="minDate" type="date" :disabled="!enableMinDate">
+          </div>
+          <div class="prop-input">
+            <label for="enableMaxDate">Enable maxDate</label>
+            <input v-model="enableMaxDate" name="enableMaxDate" type="checkbox">
+          </div>
+          <div :class="{'prop-input': true, 'disable': !enableMaxDate }">
+            <label for="maxDate">maxDate</label>
+            <input v-model="maxDate" name="maxDate" type="date" :disabled="!enableMaxDate">
+          </div>
+        </div>
+        <div class="data">
+          <code>
+            {<br>
+              "countType" : "{{ countType }}",<br>
+              "minDate" : "{{ formattedMinDate }}",<br>
+              "maxDate" : "{{ formattedMaxDate }}"<br>
+            }
+          </code>
+        </div>
       </div>
     </div>
   </main>
 </template>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 .dev-example {
+  font-family: 'sans-serif';
   display: flex;
   flex-direction: column;
-  padding: 2rem;
+  padding: 1rem;
   justify-content: center;
   align-items: center;
-}
 
-h1 {
-  margin-bottom: 2rem;
+  h1 {
+    margin-bottom: 1rem;
+  }
+
+  .props {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .form {
+      margin-right: 2rem;
+
+      .prop-input {
+        margin-bottom: 0.5rem;
+
+        &.disable {
+          opacity: 0.3;
+        }
+
+        label {
+          margin-right: 1rem;
+        }
+      }
+    }
+    .data {
+      background-color: #cecece4b;
+      padding: 1.5rem;
+    }
+  }
 }
 </style>
